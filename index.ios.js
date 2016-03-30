2c69997e66fccfd3224094ec603fb51b4617c61c
Daniel Friesen <daniel@nadir-seen-fire.com>
2016-03-29 20:34:06 -0700

    Commit bug example

---
+++ index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const ThisIsAnError = undefined;
+
+const BugTest = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Error test
+    </Text>
+    <Text style={styles.instructions}>
+      The below component generates an error.
+    </Text>
+    <ThisIsAnError />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('BugTest', () => BugTest);
